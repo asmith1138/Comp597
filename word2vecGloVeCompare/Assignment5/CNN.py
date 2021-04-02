0f1a898ed5ef53923164c2c2f72b1e792e7d589a
Andrew Smith <asmith1138@gmail.com>
2021-04-02 19:39:07 -0400

    cnn doesnt work

--- word2vecGloVeCompare/Assignment5/CNN.py
+++ word2vecGloVeCompare/Assignment5/CNN.py
@@ -18,7 +18,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
-from sklearn.neural_network import Convultion
+
+import keras
+from keras.models import Sequential,Input,Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
 
 
 import matplotlib.pyplot as plt
@@ -40,6 +46,10 @@ MIN_WORD_CNT = 40
 DOWNSAMPLING = 1E-3
 WIN_SZ = 10
 N_WRKS = 4
+
+batch_size = 64
+epochs = 20
+num_classes = 10
 
 def load_data():
     if os.path.isdir(UNSUP_DIR):
@@ -111,7 +121,7 @@ def create_review_vectors(corpus, glove_model):
     feature_vec = np.zeros((len(corpus), N_FEATURES), dtype="float32")
 
     for review in corpus:
-        feature_vec[idx] = vec_from_review(review, model)
+        feature_vec[idx] = vec_from_review(review, glove_model)
         idx = idx + 1
 
     return feature_vec
@@ -184,10 +194,57 @@ if __name__ == "__main__":
         train_corpus = prepare_corpus(data["train"], "train")
         test_corpus = prepare_corpus(data["test"], "test")
 
-    model = load_glove_model_file(GLOVE_MODEL_FILE)
 
-    train_vec, test_vec = get_vectorized_data(train_corpus, test_corpus, model)
 
-    exec_logistic_regression(train_vec, test_vec, data)
-    exec_cnn(train_vec, test_vec, data)
-    exec_deep_neural_net(train_vec, test_vec, data)
+    corpus_model = load_glove_model_file(GLOVE_MODEL_FILE)
+
+    train_vec, test_vec = get_vectorized_data(train_corpus, test_corpus, corpus_model)
+
+    #exec_logistic_regression(train_vec, test_vec, data)
+    #exec_cnn(train_vec, test_vec, data)
+    #exec_deep_neural_net(train_vec, test_vec, data)
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D((2, 2),padding='same'))
+model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Flatten())
+model.add(Dense(128, activation='linear'))
+model.add(LeakyReLU(alpha=0.1))                  
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+model.summary()
+
+train = model.fit(train_vec, data["train"]["target"][:N_ITEMS], batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(test_vec, data["test"]["target"][:N_ITEMS]))
+
+
+test_eval = model.evaluate(test_vec, data["test"]["target"][:N_ITEMS], verbose=0)
+
+print('Test loss:', test_eval[0])
+print('Test accuracy:', test_eval[1])
+
+
+accuracy = train.history['acc']
+val_accuracy = train.history['val_acc']
+loss = train.history['loss']
+val_loss = train.history['val_loss']
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'bo', label='Training accuracy')
+plt.plot(epochs, val_accuracy, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
